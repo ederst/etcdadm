b1e37b250ddd239a1d7a048f2cb20670e31ce91c
ederst <stefan.prietl@bearingpoint.com>
2022-08-08 13:23:44 +0200

    Filter fixed IP addresses with CIDR notation
    This allows filtering the fixed IP addresses returned by the OpenStack
    compute API with a filter defined as CIDR notation.
    
    When specifying the filter then the first fixed IP address matching the
    CIDR notation will be returned.
    
    If no filter is specified then the first found IP will be returned.

--- etcd-manager/pkg/volumes/openstack/volumes.go
+++ etcd-manager/pkg/volumes/openstack/volumes.go
@@ -74,6 +74,7 @@ type OpenstackVolumes struct {
 
 	matchTagKeys []string
 	matchTags    map[string]string
+	networkCIDRs []*net.IPNet
 
 	computeClient *gophercloud.ServiceClient
 	volumeClient  *gophercloud.ServiceClient
@@ -97,7 +98,7 @@ type MetadataService struct {
 var _ volumes.Volumes = &OpenstackVolumes{}
 
 // NewOpenstackVolumes builds a OpenstackVolume
-func NewOpenstackVolumes(clusterName string, volumeTags []string, nameTag string) (*OpenstackVolumes, error) {
+func NewOpenstackVolumes(clusterName string, volumeTags []string, nameTag string, networkCIDRs []*net.IPNet) (*OpenstackVolumes, error) {
 
 	metadata, err := getLocalMetadata()
 	if err != nil {
@@ -105,10 +106,11 @@ func NewOpenstackVolumes(clusterName string, volumeTags []string, nameTag string
 	}
 
 	stack := &OpenstackVolumes{
-		clusterName: clusterName,
-		meta:        metadata,
-		matchTags:   make(map[string]string),
-		nameTag:     nameTag,
+		clusterName:  clusterName,
+		meta:         metadata,
+		matchTags:    make(map[string]string),
+		nameTag:      nameTag,
+		networkCIDRs: networkCIDRs,
 	}
 
 	for _, volumeTag := range volumeTags {
@@ -369,7 +371,7 @@ func (stack *OpenstackVolumes) discoverTags() error {
 		if mc.ObserveRequest(err) != nil {
 			return fmt.Errorf("failed to retrieve server information from cloud: %v", err)
 		}
-		ip, err := GetServerFixedIP(extendedServer.Addresses, extendedServer.Name)
+		ip, err := GetServerFixedIP(extendedServer.Addresses, extendedServer.Name, stack.networkCIDRs)
 		if err != nil {
 			return fmt.Errorf("error querying InternalIP from name: %v", err)
 		}
